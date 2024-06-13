3306c6471b285584644b2aa506b236b4ae005132
Guilherme <cechinelguilherme@gmail.com>
2024-06-13 01:23:25 -0300

    terminando os resultados

--- js/script.js
+++ js/script.js
@@ -1,13 +1,48 @@
 const inputTurns = document.getElementById("turns");
 const inputBonus = document.getElementById("bonus");
+const resultDiv = document.getElementById("resultDiv");
+const result = document.querySelector(".result");
 
 const inputValue = () => {
   const value = {
-    turns: inputTurns.value,
-    bonus: inputBonus.value,
+    turns: Number(inputTurns.value),
+    bonus: Number(inputBonus.value),
   };
 
   return value;
+};
+
+const showResult = (num, total, array, bonus) => {
+  const img = document.createElement("img");
+  const p = document.createElement("p");
+  const h3 = document.createElement("h3");
+
+  img.src = `images/d${num.toString()}.png`;
+
+  if (array.length === 1) {
+    p.innerHTML = ``;
+  } else if (bonus === 0) {
+    p.innerHTML = `{${array}}`;
+  } else {
+    p.innerHTML = `{${array}} + ${bonus}`;
+  }
+
+  h3.innerHTML = total;
+
+  result.appendChild(img);
+  result.appendChild(p);
+  result.appendChild(h3);
+
+  resultDiv.classList.remove("hidden");
+
+  setTimeout(() => {
+    resultDiv.classList.add("hidden");
+    result.removeChild(img);
+    result.removeChild(p);
+    result.removeChild(h3);
+  }, 2500);
+
+  return;
 };
 
 const rolld = (num) => {
@@ -25,8 +60,9 @@ const rolld = (num) => {
   for (let i = 0; i < arrayNumber.length; i++) {
     total += arrayNumber[i];
   }
- 
-  
 
-  return arrayNumber, total;
+  total += data.bonus;
+
+  showResult(num, total, arrayNumber, data.bonus);
+  return;
 };
